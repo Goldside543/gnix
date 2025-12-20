43a24d4e8a9e63bcc3289deb1841bbbe70e722e5
Goldside543 <goldside000@outlook.com>
2025-12-19 23:30:50 -0500

    Refactor kernel.s

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,9 +1,10 @@
 .code16
+.org 0x8000
 .global _start
 
 _start:
     cli
-    xorw %ax, %ax
+    movw $0x8000, %ax
     movw %ax, %ds
     movw %ax, %es
     sti
@@ -11,21 +12,19 @@ _start:
     movw $message, %si
 
 print_loop:
-    lodsb                  # AL = *SI++
+    lodsb
     testb %al, %al
     jz done
-
-    movb $0x0E, %ah         # teletype output
-    movb $0x00, %bh         # page
-    movb $0x07, %bl         # attribute
+    movb $0x0E, %ah
+    movb $0x00, %bh
+    movb $0x07, %bl
     int $0x10
     jmp print_loop
 
 done:
     cli
-halt:
-    nop
-    jmp nop
 
 message:
     .asciz "Hello, world!"
+
+.fill 512 - (. - _start), 1, 0
