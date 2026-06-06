96337153296dcf585b23724029f8d357718836c4
Goldside543 <goldside000@outlook.com>
2026-06-05 23:53:56 -0400

    Add test.c

---
+++ hlibc/test.c
@@ -0,0 +1,16 @@
+int main() {
+    putc("H");
+    putc("e");
+    putc("l");
+    putc("l");
+    putc("o");
+    putc(" ");
+    putc("f");
+    putc("r");
+    putc("o");
+    putc("m");
+    putc(" ");
+    putc("C");
+    putc("!");
+    putc("\n");
+}
