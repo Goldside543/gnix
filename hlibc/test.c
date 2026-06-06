83910b5f3217bb4adc4df9e835bfc7cc08589858
Goldside543 <goldside000@outlook.com>
2026-06-06 00:20:31 -0400

    Change quotes in putc function calls

--- hlibc/test.c
+++ hlibc/test.c
@@ -1,16 +1,16 @@
 int main() {
-    putc("H");
-    putc("e");
-    putc("l");
-    putc("l");
-    putc("o");
-    putc(" ");
-    putc("f");
-    putc("r");
-    putc("o");
-    putc("m");
-    putc(" ");
-    putc("C");
-    putc("!");
-    putc("\n");
+    putc('H');
+    putc('e');
+    putc('l');
+    putc('l');
+    putc('o');
+    putc(' ');
+    putc('f');
+    putc('r');
+    putc('o');
+    putc('m');
+    putc(' ');
+    putc('C');
+    putc('!');
+    putc('\n');
 }
